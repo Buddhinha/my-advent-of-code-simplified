e85a757f1b116615417048c32f9584caeb590944
Arthur <153740966+Buddhinha@users.noreply.github.com>
2024-12-04 21:17:45 -0300

    Delete advent of code day 6.php

--- advent of code day 6.php
+++
@@ -1,7 +0,0 @@
-<?php  
-	$num = readline("digite um numero");
-	for ($multiplicador =1; $multiplicador <= 10 ; $multiplicador++) { 
-		$final = $num*$multiplicador;
-		echo"a multiplicação de $num veses $multiplicador é $final \n";
-	}
-?>
