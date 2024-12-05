73e951141270e393c871b82be94f275c5b27d5c0
Arthur <153740966+Buddhinha@users.noreply.github.com>
2024-12-04 21:18:26 -0300

    Create adventOfCodeDay5.php

---
+++ adventOfCodeDay5.php
@@ -0,0 +1,8 @@
+<?php  
+	$num = readline("digite um numero");
+	for ($multiplicador =1; $multiplicador <= 10 ; $multiplicador++) { 
+		$final = $num*$multiplicador;
+		echo"a multiplicação de $num veses $multiplicador é $final \n";
+	}
+
+?>
