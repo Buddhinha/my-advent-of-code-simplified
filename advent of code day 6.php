ed46adfbb6c33d0958f1aff81e63b328c7568b0d
Arthur <153740966+Buddhinha@users.noreply.github.com>
2024-12-11 21:29:27 -0300

    Create advent of code day 6.php

---
+++ advent of code day 6.php
@@ -0,0 +1,5 @@
+<?php
+$string = readline("digite uma palavra e sera retornado o número de caracteres da string\n");
+$stringContada =strlen($string);
+echo "Esse é o número de caracteres da palavra $string é $stringContada \n";
+
